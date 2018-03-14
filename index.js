e838d7b8751750533d3444ec2c2c9b36d5ae3379
Jhosehp <jhosehp.rendon@gmail.com>
2018-03-14 18:45:19 -0500

    Fixed server setup

--- index.js
+++ index.js
@@ -22,5 +22,9 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
+app.get('/', (req, res) =>{
+    res.send({hi: 'there'})
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT); 
