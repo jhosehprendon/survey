c89ec1f890f1bf734cdd7a8abb48d8cb3b54bdc2
Jhosehp <jhosehp.rendon@gmail.com>
2018-03-15 06:36:02 -0500

    Deleted keys in gitignore

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    googleClientID: '79043209635-k3lsbahe983fhfgb382fjkiefnchlluq.apps.googleusercontent.com',
+    googleClientSecret: 'Pov5EWEfqGFdzxlnmESFocQs',
+    mongoURI: 'mongodb://jhosehp:123@ds213199.mlab.com:13199/survey-dev',
+    cookieKey: 'jdskcndkcnkdnkdnoooo'
+};
+
