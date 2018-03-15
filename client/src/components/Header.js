5bae4f4c30dd27928aecb8bd5f02dde3970889a7
Jhosehp <jhosehp.rendon@gmail.com>
2018-03-15 18:36:54 -0500

    Finished Authentication in client side and header feature

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
+    renderContent(){
+        switch(this.props.auth) {
+            case null:
+                return;
+            case false:
+                return (
+                    <li><a href='/auth/google'>Log in with Google</a></li>
+                );
+            default:
+                return <li><a href='/api/logout'>Logout</a></li>;
+        }
+    }
+
     render(){
         return(
             <nav>
                 <div className="nav-wrapper">
-                    <a className="left brand-logo">Survey</a>
+                    <Link 
+                        to={this.props.auth ? '/surveys' : '/'} 
+                        className="left brand-logo">
+                    Survey</Link>
                     <ul className="right">
-                        <li>
-                            <a>Login with Google</a>
-                        </li>    
+                        {this.renderContent()}  
                     </ul>    
                 </div>
             </nav>
@@ -17,4 +33,8 @@ class Header extends Component {
     }
 }
 
-export default Header;
+function mapStateToProps(state){
+    return {auth: state.auth};
+}
+
+export default connect(mapStateToProps)(Header);
