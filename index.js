ea4d78583bd87c889ec3cd6b48cbef06f411d712
Jhosehp <jhosehp.rendon@gmail.com>
2018-03-16 11:36:07 -0500

    Created Survey model, routes and made sure credits are enough

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
 require('./models/User');
+require('./models/Survey')
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
@@ -25,6 +26,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if(process.env.NODE_ENV === 'production') {
     // express will serve up production assets like our main.js file, or main.css file
