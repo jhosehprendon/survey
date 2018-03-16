2d5f25b2aaf67347341daba0cf9113108748892d
Jhosehp <jhosehp.rendon@gmail.com>
2018-03-16 07:53:36 -0500

    Integrated Stripe payment option to client and server side

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
     renderContent(){
@@ -11,8 +12,11 @@ class Header extends Component {
                 return (
                     <li><a href='/auth/google'>Log in with Google</a></li>
                 );
-            default:
-                return <li><a href='/api/logout'>Logout</a></li>;
+            default: 
+                return [
+                <li key="1"><Payments /></li>,    
+                <li key="2"><a href='/api/logout'>Logout</a></li>
+            ];
         }
     }
 
--- config/dev.js
+++ config/dev.js
@@ -3,5 +3,7 @@ module.exports = {
     googleClientID: '79043209635-k3lsbahe983fhfgb382fjkiefnchlluq.apps.googleusercontent.com',
     googleClientSecret: 'Pov5EWEfqGFdzxlnmESFocQs',
     mongoURI: 'mongodb://jhosehp:123@ds213199.mlab.com:13199/survey-dev',
-    cookieKey: 'jdskcndkcnkdnkdnoooo'
+    cookieKey: 'jdskcndkcnkdnkdnoooo',
+    stripePublishableKey: "pk_test_Mwi8eAKeQvWtDsbATdeZo047",
+    stripeSecretKey: "sk_test_L4g9NSJbrj7nSdfXu0I5beDZ"
 };
