6115f497d5be8c50e0300224557dd17ead2b39b5
Jhosehp <jhosehp.rendon@gmail.com>
2018-03-17 17:38:34 -0500

    Added form validation and error message to user

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
+import validateEmails from '../../utils/validateEmails'
 
 class SurveyForm extends Component {
     renderFields(){
@@ -20,13 +22,38 @@ class SurveyForm extends Component {
             <div>
                 <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
                     {this.renderFields()}
-                    <button type="submit">Submit</button>
+                    <Link to="/surveys" className="red btn-flat white-text">Cancel</Link>
+                    <button className="teal btn-flat right white-text" type="submit">
+                        Next 
+                        <i className="material-icons right">done</i>
+                    </button>
                 </form>
             </div>
         );
     }
 }
 
+function validate (values) {
+    const errors = {};
+
+    if (!values.title) {
+        errors.title = 'You must provide a title';
+    }
+
+    if (!values.subject) {
+        errors.subject = 'You must provide a subject';
+    }
+
+    if (!values.body) {
+        errors.body = 'You must provide a body text';
+    }
+
+    errors.emails = validateEmails(values.emails || '');
+
+    return errors;
+}
+
 export default reduxForm({
+    validate,
     form: 'surveyForm'
 })(SurveyForm);
