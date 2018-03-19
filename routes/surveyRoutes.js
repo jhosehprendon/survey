947157f0a173d07e8f31bc5a54f6337679ebced7
Jhosehp <jhosehp.rendon@gmail.com>
2018-03-18 22:01:05 -0500

    Finished handling webhook data and added LocalTunnel setup

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const Path = require('path-parser');
+const {URL} = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -8,8 +11,23 @@ const Survey = mongoose.model('surveys');
 
 module.exports = app => {
 
-    app.post('/api/surveys/thanks', (req, res) => {
+    app.get('/api/surveys/thanks', (req, res) => {
         res.send('Thanks for your feedback')
+    });
+
+    app.post('/api/surveys/webhooks', (req, res) => {
+        const p = new Path('/api/surveys/:surveyId/:choice');
+
+        const events = _.map(req.body, ({email, url}) => { 
+            const match = p.test(new URL(url).pathname);
+            if (match){
+                return { email, surveyId: match.surveyId, choice: match.choice}
+            }
+        });
+        const compactEvents = _.compact(events);
+        const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyid');
+
+        res.send({});
     })
 
     app.post('/api/surveys', requireLogin, requireCredits, async (req, res) =>{
@@ -30,7 +48,7 @@ module.exports = app => {
         await mailer.send();
         await survey.save();
         req.user.credits -=1;
-        const user = await equal.user.save();
+        const user = await req.user.save();
 
         res.send(user);
        } catch (err) {
